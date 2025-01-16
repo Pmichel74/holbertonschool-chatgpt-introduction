5ac5420190a50d71a960b344d0cdd4c101f1d650
Pmichel74 <patrick.yann.michelgmail.com>
2025-01-16 09:51:31 +0100

    push all task

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -2,11 +2,13 @@
 import sys
 
 def factorial(n):
+    """Calculate the factorial of a number."""
     result = 1
     while n > 1:
         result *= n
         n -= 1
     return result
 
-f = factorial(int(sys.argv[1]))
-print(f)
+if __name__ == "__main__":
+    f = factorial(int(sys.argv[1]))
+    print(f)
